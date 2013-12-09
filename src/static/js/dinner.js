8af853610d077cc372d401e4f79e0ea8e38a3ce3
Micfan <micfan@users.noreply.github.com>
2013-12-09 23:13:28 +0800

    just use jQuery, but Vue.js

--- src/static/js/dinner.js
+++ src/static/js/dinner.js
@@ -3,52 +3,38 @@
 // Micheal Fan, 2015-03-23
 
 // 配置Vue变量标签
-Vue.config.delimiters = ["[", "]"];
+/*Vue.config.delimiters = ["[", "]"];
 
 Vue.directive('disable', function (value) {
     this.el.disabled = !!value;
 });
-/*
-$.ajax({
-  url: url,
-  type: 'post',
-  dataType: 'json',
-  data: pdata,
-  success: function(response) {
-    data.message = response.detail;
-  }
-});
 */
+"use strict";
 
-var test = new Vue({
-  el: '#test0',
-  data: {
-    selected: false
-  },
-  methods: {
-        onClick: function (e) {
-            // console.log(e.target.tagName) // "A"
-            // console.log(e.targetVM === this) // true
-            // e是原生的DOM事件对象
-            // this 指向该ViewModel实例
-            var self = this;
-            var url = '/dinner/';  // todo: js动态URL
-            var pdata = {
-              selected: Number(this.selected),
-              cal_id: 84
-            };
-            // todo: jQuery.Ajax能利用Http StatusCode吗?
-            // todo: Loading菊花。。
-            $.post(url, pdata, function(response) {
-              if (response.ec === 0) {
-                self.selected = !self.selected;
-                $('#order_count').html(response.data);
-              } else {
-                // todo: 显示错误提示
-                ///
-                ///
-              }
-            }, 'json');
-        }
-    }
+$(function () {
+
+  function toggleBookStatus($el) {
+    // todo: 动态URL
+    var url = '/dinner/';
+    var hasBooked = $el.hasClass('has_booked') ? 1 : 0;
+    var data = {
+      has_booked: hasBooked,
+      cal_id: $el.attr('cal-id')
+    };
+    $.post(url, data, function(response) {
+      if (response.ec === 0) {
+        $el.toggleClass('has_booked');
+        $('#order_count').html(response.data);
+      }
+    });
+  }
+
+  (function main() {
+
+    $('.workday').on('click', function() {
+      toggleBookStatus($(this));
+    });
+
+  })();
 });
+
