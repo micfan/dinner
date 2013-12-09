7a66ab6e1acecbdf905788ecc04d7dd0d7a8054f
Micfan <micfan@users.noreply.github.com>
2013-12-09 23:13:28 +0800

    version 0.0.1
    Basic Django framework, static files, no models defined

--- src/apps/public/urls.py
+++ src/apps/public/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, url, include
 from src.settings.base import LOGIN_URL, LOGOUT_URL
 
 urlpatterns = patterns('src.apps.public.views',
-  url(r'^$', 'index'),
+  url(r'^$', 'index', {'tpl': 'public/index.html'}),
   url(r'^%s' % LOGIN_URL[1:], 'login', {'tpl': 'public/login.html'}),
   url(r'^%s' % LOGOUT_URL[1:], 'logout'),
+  url(r'^h/(?P<tpl_prefix>\w*)', 'html'),
 )
