9ba9739cd5e7de9ec0a75182c4903e1779f9550e
mic <fanlinsheng@gmail.com>
2014-07-25 14:38:06 +0800

    version 0.0.1
    Basic Django framework, static files, no models defined

--- src/apps/public/views.py
+++ src/apps/public/views.py
@@ -6,11 +6,12 @@ except:
   import json
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
-@login_required
-def index(request):
-  """Home page"""
 
-  return HttpResponse(content='Hello, world!', status=200, content_type='text/html')
+
+def index(request, tpl):
+  """Home page"""
+  return render(request, tpl, {})
+  # return HttpResponse(content='Hello, world!', status=200, content_type='text/html')
 
 def login(request, tpl):
   """"""
@@ -19,7 +20,12 @@ def login(request, tpl):
   # js = {'status': 1, 'message': 'ok', 'ec': 0}
   # return HttpResponse(content=json.dumps(js), status=200, content_type='application/json')
 
+@login_required
 def logout(request):
   """"""
   js = {'status': 1, 'message': 'ok', 'ec': 0}
   return HttpResponse(content=json.dumps(js), status=200, content_type='application/json')
+
+def html(request, tpl_prefix):
+  tpl = 'html/%s.html' % tpl_prefix
+  return render(request, tpl)
