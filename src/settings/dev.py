0f6a99573e277f60dc3316c5ca237f2d3704c582
MichaelFan <fanlinsheng@gmail.com>
2015-05-18 23:36:06 +0800

    remove Baidu CDN migrate to django_compressor

--- src/settings/dev.py
+++ src/settings/dev.py
@@ -5,7 +5,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 # django_compressor
-COMPRESS_ENABLED = not True
+COMPRESS_ENABLED = True
 
 MEDIA_ROOT = os.path.join(VAR_ROOT, 'uploads')
 STATIC_ROOT = os.path.join(VAR_ROOT, 'static')
